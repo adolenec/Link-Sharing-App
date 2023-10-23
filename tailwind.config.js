72afc9690f4e97f0d85f9dcea20df6322d447f87
adolenec <andrija.dolenec23@gmail.com>
2023-10-23 17:15:15 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      "green-primary": "#C6D752",
+      "green-secondary": "#D7E472",
+      "green-ghost": "#E9F0A6",
+      "black-primary": "#333333",
+      "gray-primary": "737373",
+      "gray-secondary": "#D9D9D9",
+      "gray-ghost": "#FAFAFA",
+      white: "#FFFFFF",
+      red: "#FF3939",
+    },
+    extend: {},
+  },
+  plugins: [],
+};

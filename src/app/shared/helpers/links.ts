111078159be9c16405c21f7f4e325de3eb3988e7
adolenec <andrija.dolenec23@gmail.com>
2023-10-25 08:29:11 +0200

    Add link svg icons

---
+++ src/app/shared/helpers/links.ts
@@ -0,0 +1,14 @@
+export const LINKS = [
+  { id: 1, name: 'GitHub' },
+  { id: 2, name: 'Dev.to' },
+  { id: 3, name: 'Twitter' },
+  { id: 4, name: 'Codewars' },
+  { id: 5, name: 'LinkedIn' },
+  { id: 6, name: 'freeCodeCamp' },
+  { id: 7, name: 'YouTube' },
+  { id: 8, name: 'GitLab' },
+  { id: 9, name: 'Facebook' },
+  { id: 10, name: 'Hashnode' },
+  { id: 11, name: 'Twitch' },
+  { id: 12, name: 'Stack Overflow' },
+];

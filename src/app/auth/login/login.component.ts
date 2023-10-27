c6676214492e41ecbb1003ee30b2ebd4fe907535
adolenec <andrija.dolenec23@gmail.com>
2023-10-27 14:58:41 +0200

    Add login and register pages

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { InputTextModule } from 'primeng/inputtext';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AngularSvgIconModule, InputTextModule, RouterLink],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { InputTextModule } from 'primeng/inputtext';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AngularSvgIconModule,
+    RouterLink,
+    InputTextModule,
+  ],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {}
